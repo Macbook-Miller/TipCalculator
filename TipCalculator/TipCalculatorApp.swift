e8960318e756ce28ef47489ec3b65a1d81b0a779
Macbook-Miller <frederikvaleur@icloud.com>
2025-08-12 11:19:01 +0200

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Fred on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
